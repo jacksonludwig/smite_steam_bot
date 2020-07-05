8baa65678f4452abd85ba31764e7ec3487e50b78
Jackson <jacksonludwig0@gmail.com>
2020-07-04 20:35:25 -0400

    Start trying to get next sibling

--- src/retriever.rs
+++ src/retriever.rs
@@ -1,11 +1,18 @@
 use scraper::{Html, Selector};
 
+// TODO: return a vector of the items.
 pub fn scrape_starter<'a>(soup: &'a Html) -> () {
-    let div_selector = Selector::parse(".pro-build").unwrap();
+    let div_selector = Selector::parse("div.pro-build__title").unwrap();
+
+    // TODO: lower loop prob redone so that we just check next sibling's children's children's alts.
 
     println!("filtering started");
     for item in soup.select(&div_selector) {
-        let item_text = item.text().filter(|x| x != &" ").collect::<Vec<_>>();
+        let item_text = item
+            .text()
+            .filter(|x| x != &" ")
+            .take_while(|x| x != &"Final Build")
+            .collect::<Vec<_>>();
         println!("{:?}", item_text);
     }
 }
