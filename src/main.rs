c5cc777cb7c0f0842e2c99d78881b6247ea9a59e
Jackson <jacksonludwig0@gmail.com>
2020-07-05 13:25:31 -0400

    basic start and end fetch done

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use soup::prelude::*;
 
 fn main() -> Result<(), isahc::Error> {
     let mut webpage = isahc::get("https://smite.guru/builds/anubis")?;
-    let soup = Soup::from_reader(webpage.text()?.as_bytes());
+    let soup = Soup::from_reader(webpage.text()?.as_bytes())?;
 
     println!("Scraping started");
 
-    let starter_items = retriever::scrape_starter(&soup.unwrap());
+    let starter_items = retriever::scrape_starter(&soup);
+    let ending_items = retriever::scrape_ending(&soup);
+
     println!("{:?}", starter_items);
+    println!("{:?}", ending_items);
 
     Ok(())
 }
--- src/retriever.rs
+++ src/retriever.rs
@@ -1,16 +1,28 @@
 use soup::prelude::*;
 
-pub fn scrape_starter(soup: &Soup) -> Vec<String> {
+const BUILD_DIV_TAG: &str = "data-v-c00756d0";
+const STARTER_BUILD_INDEX: usize = 6;
+const ENDING_BUILD_INDEX: usize = 16;
+
+pub fn scrape_build(soup: &Soup, build_index: usize) -> Vec<String> {
     let pro_div = soup
         .tag("div")
-        .attr("data-v-c00756d0", "")
+        .attr(BUILD_DIV_TAG, "")
         .find_all()
-        .take_while(|e| &e.text() != &"Final Build")
+        .take_while(|e| &e.text() != &"Leveling Order")
         .collect::<Vec<_>>();
 
-    pro_div[6]
+    pro_div[build_index]
         .tag("img")
         .find_all()
         .map(|e| e.get("alt").unwrap())
         .collect::<Vec<_>>()
+}
+
+pub fn scrape_starter(soup: &Soup) -> Vec<String> {
+    scrape_build(soup, STARTER_BUILD_INDEX)
+}
+
+pub fn scrape_ending(soup: &Soup) -> Vec<String> {
+    scrape_build(soup, ENDING_BUILD_INDEX)
 }
