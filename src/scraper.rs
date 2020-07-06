87ad283f068f8a8b22c6ccb2c672dccc8a78f5fd
Jackson <jacksonludwig0@gmail.com>
2020-07-05 20:29:47 -0400

    scraper hopefully done

--- src/scraper.rs
+++ src/scraper.rs
@@ -10,21 +10,6 @@ const ENDING_RELIC_OFFSET: isize = 6;
 
 const BUILD_DIV_TAG: &str = "data-v-c00756d0";
 
-fn scrape_build(soup: &Soup, build_index: isize) -> Vec<String> {
-    let pro_div = soup
-        .tag("div")
-        .attr(BUILD_DIV_TAG, "")
-        .find_all()
-        .take_while(|e| &e.text() != &"Leveling Order")
-        .collect::<Vec<_>>();
-
-    pro_div[build_index as usize]
-        .tag("img")
-        .find_all()
-        .map(|e| e.get("alt").unwrap())
-        .collect::<Vec<_>>()
-}
-
 struct Builder {
     soup: Soup,
 }
@@ -35,15 +20,31 @@ impl Builder {
         Builder { soup }
     }
 
+    fn scrape_build(&self, build_index: isize) -> Vec<String> {
+        let pro_div = self
+            .soup
+            .tag("div")
+            .attr(BUILD_DIV_TAG, "")
+            .find_all()
+            .take_while(|e| &e.text() != &"Leveling Order")
+            .collect::<Vec<_>>();
+
+        pro_div[build_index as usize]
+            .tag("img")
+            .find_all()
+            .map(|e| e.get("alt").unwrap())
+            .collect::<Vec<_>>()
+    }
+
     fn scrape_beg_and_end(&self) -> Vec<Vec<String>> {
-        let starter = scrape_build(&self.soup, STARTER_BUILD_INDEX);
+        let starter = self.scrape_build(STARTER_BUILD_INDEX);
 
         let end_index = ENDING_BUILD_INDEX - (STARTER_ITEM_AMT_DEFAULT - (starter.len() as isize));
-        let mut ending = scrape_build(&self.soup, end_index);
+        let mut ending = self.scrape_build(end_index);
 
         let end_relic_index =
             end_index + ENDING_RELIC_OFFSET + (ending.len() as isize) - ENDING_ITEM_AMT_DEFAULT;
-        let mut ending_relics = scrape_build(&self.soup, end_relic_index);
+        let mut ending_relics = self.scrape_build(end_relic_index);
 
         ending.append(&mut ending_relics);
 
